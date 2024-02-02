9d2077479978502b6fe1baafb199c692b63a541d
Falleiro <caiofalleiro@gmail.com>
2024-02-02 16:07:43 -0300

    corrigindo bugs

--- apps/galeria/forms.py
+++ apps/galeria/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from apps.galeria.models import Fotografia
 
 class FotografiaForms(forms.ModelForm):
@@ -6,23 +7,23 @@ class FotografiaForms(forms.ModelForm):
         model = Fotografia
         exclude = ['publicada',]
         labels = {
-            'descricao': 'Descrição',
-            'data_fotografia': 'Data de Registro',
-            'usuario': 'Usuário'
+            'descricao':'Descrição',
+            'data_fotografia': 'Data de registro',
+            'usuario': 'Usuário',
         }
-
+    
         widgets = {
-            'nome':forms.TextInput(attrs={'class': 'fomr-control'}),
-            'legenda':forms.TextInput(attrs={'class': 'fomr-control'}),
-            'categoria':forms.Select(attrs={'class': 'fomr-control'}),
-            'descricao':forms.Textarea(attrs={'class': 'fomr-control'}),
-            'foto':forms.FileInput(attrs={'class': 'fomr-control'}),
-            'data_fotografia':forms.DateInput(
-                format='%d/%m/%Y',
+            'nome': forms.TextInput(attrs={'class':'form-control'}),
+            'legenda': forms.TextInput(attrs={'class':'form-control'}),
+            'categoria': forms.Select(attrs={'class':'form-control'}),
+            'descricao': forms.Textarea(attrs={'class':'form-control'}),
+            'foto': forms.FileInput(attrs={'class':'form-control'}),
+            'data_fotografia': forms.DateInput(
+                format = '%d/%m/%Y',
                 attrs={
-                    'type': 'date',
-                    'class': 'fomr-control'
-                    }
-                ),
-            'usuario':forms.Select(attrs={'class': 'fomr-control'}),
+                    'type':'date',
+                    'class':'form-control'
+                }
+            ),
+            'usuario': forms.Select(attrs={'class':'form-control'}),
         }
--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
 from apps.galeria.models import Fotografia
-from django.contrib import messages
 from apps.galeria.forms import FotografiaForms
 
+from django.contrib import messages
 
 def index(request):
     if not request.user.is_authenticated:
         messages.error(request, 'Usuário não logado')
         return redirect('login')
-    
+
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
-    
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
@@ -22,13 +22,12 @@ def buscar(request):
         messages.error(request, 'Usuário não logado')
         return redirect('login')
 
-
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+
     if "buscar" in request.GET:
         nome_a_buscar = request.GET['buscar']
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
-
 
     return render(request, "galeria/buscar.html", {"cards": fotografias})
 
@@ -36,23 +35,19 @@ def nova_imagem(request):
     if not request.user.is_authenticated:
         messages.error(request, 'Usuário não logado')
         return redirect('login')
-    
-    form = FotografiaForms()
+
+    form = FotografiaForms
     if request.method == 'POST':
         form = FotografiaForms(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Foto cadastrada com sucesso')
+            messages.success(request, 'Nova fotografia cadastrada!')
             return redirect('index')
-            
-    return render (request, "galeria/nova_imagem.html", {'form': form})
-    
 
+    return render(request, 'galeria/nova_imagem.html', {'form': form})
 
 def editar_imagem(request):
-    return render (request, "galeria/editar_imagem.html")
+    pass
 
 def deletar_imagem(request):
-    return render (request, "galeria/deletar_imagem.html")
-
-
+    pass
